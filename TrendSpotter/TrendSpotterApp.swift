66207edcdf00b7202f10c42f31d4f159bba2bfc9
SivaKalai <sivakalaikshan1999@gmail.com>
2024-03-16 09:23:13 +0530

    Initial Commit

---
+++ TrendSpotter/TrendSpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendSpotterApp.swift
+//  TrendSpotter
+//
+//  Created by NIBM on 2024-03-16.
+//
+
+import SwiftUI
+
+@main
+struct TrendSpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

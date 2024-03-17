487af7f1f60fd4e1652283f4757ef81ff6e53e4e
Sivakalaikshan <sivakalaikshan1999@gmail.com>
2024-03-17 16:38:26 +0530

    create navigation bar, home page and profile page 2024/03/17

---
+++ TrendSpotter/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TrendSpotter
+//
+//  Created by NIBM on 2024-03-17.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ TrendSpotter/Views/NavigationBarView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBarView.swift
+//  TrendSpotter
+//
+//  Created by NIBM on 2024-03-17.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}

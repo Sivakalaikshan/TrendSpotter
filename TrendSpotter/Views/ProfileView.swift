dccd82fb2ab83a91fe3f0dfb85b88759c4a02f49
Sivakalaikshan <sivakalaikshan1999@gmail.com>
2024-03-21 19:49:24 +0530

    profile design 2024-03-21

--- TrendSpotter/Views/ProfileView.swift
+++ TrendSpotter/Views/ProfileView.swift
@@ -23,87 +23,107 @@ struct ProfileView: View {
             ScrollView {
                 VStack {
                    
+                    VStack {
                         Text("User Name")
-                            .padding()
-                       
-                    
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(lineWidth: 0.5)
-                        .frame(width: 350, height: 50)
-                        .padding(.horizontal, 48)
-                        .overlay {
-                            HStack {
-                                TextField("Username", text: $name)
-                            }
-                            .padding(.horizontal, 56)
-                        }
-                    
-                    
-                        Text("Email Address")
-                            .padding()
-                        
-                    
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(lineWidth: 0.5)
-                        .frame(width: 350, height: 50)
-                        .padding(.horizontal, 48)
-                        .overlay {
-                            HStack {
-                                TextField("email", text: $name)
-                            }
-                            .padding(.horizontal, 56)
-                        }
-                    
-                    
-                        Text("Date Of Birth")
-                            .padding()
-                        Spacer()
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(lineWidth: 0.5)
-                        .frame(width: 350, height: 50)
-                        .padding(.horizontal, 48)
-                        .overlay {
-                            HStack {
-                                TextField("Select a date", text: $dateString)
-                                    .padding()
-                                    .onTapGesture {
-                                        isDatePickerVisible.toggle()
-                                    }
-                                if isDatePickerVisible {
-                                    DatePicker("", selection: $selectedDate, displayedComponents: .date)
-                                        .datePickerStyle(GraphicalDatePickerStyle())
-                                        .labelsHidden()
-                                        .frame(maxHeight: 400)
-                                        .padding()
-                                        .background(Color.white)
-                                        .cornerRadius(10)
-                                        .shadow(radius: 5)
-                                        .onChange(of: selectedDate) { _ in
-                                            dateString = formatDate(selectedDate)
-                                            isDatePickerVisible = false
-                                        }
+                            .padding(.trailing,250)
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(lineWidth: 0.5)
+                            .frame(width: 350, height: 50)
+                            .padding(.horizontal, 48)
+                            .overlay {
+                                HStack {
+                                    TextField("Username", text: $name)
                                 }
+                                .padding(.horizontal, 56)
                             }
-                            .padding()
-                            .padding(.horizontal, 56)
-                        }
+                    }.padding(.bottom,5)
+                   
+                    
+                    
+                    VStack {
+                        Text("Email Address")
+                            .padding(.trailing,230)
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(lineWidth: 0.5)
+                            .frame(width: 350, height: 50)
+                            .padding(.horizontal, 48)
+                            .overlay {
+                                HStack {
+                                    TextField("email", text: $name)
+                                }
+                                .padding(.horizontal, 56)
+                            }
+                    }.padding(.bottom,5)
+                    
+                    
+                    
+                    VStack {
+                        Text("Date Of Birth")
+                            .padding(.trailing,240)
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(lineWidth: 0.5)
+                            .frame(width: 350, height: 50)
+                            .padding(.horizontal, 48)
+                            .overlay {
+                                HStack {
+                                    TextField("Select a date", text: $dateString)
+                                        .padding()
+                                        .onTapGesture {
+                                            isDatePickerVisible.toggle()
+                                        }
+                                    if isDatePickerVisible {
+                                        DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                                            .datePickerStyle(GraphicalDatePickerStyle())
+                                            .labelsHidden()
+                                            .frame(maxHeight: 400)
+                                            .padding()
+                                            .background(Color.white)
+                                            .cornerRadius(10)
+                                            .shadow(radius: 5)
+                                            .onChange(of: selectedDate) { _ in
+                                                dateString = formatDate(selectedDate)
+                                                isDatePickerVisible = false
+                                            }
+                                    }
+                                }
+                                .padding(.horizontal, 56)
+                            }
+                    }.padding(.bottom,5)
+                    
                     
                    
+                    VStack{
                         Text("Password")
-                            .padding()
-                    RoundedRectangle(cornerRadius: 15)
-                        .stroke(lineWidth: 0.5)
-                        .frame(width: 350, height: 50)
-                        .padding(.horizontal, 48)
-                        .overlay {
-                            HStack {
-                                TextField("password", text: $name)
+                            .padding(.trailing,260)
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(lineWidth: 0.5)
+                            .frame(width: 350, height: 50)
+                            .padding(.horizontal, 48)
+                            .overlay {
+                                HStack {
+                                    TextField("password", text: $name)
+                                }
+                                .padding(.horizontal, 56)
                             }
-                            .padding(.horizontal, 56)
-                        }
+                    }.padding(.bottom,5)
+                    
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("Done")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .cornerRadius(10)
+                    }
+
+
+                        
+                    
                 }
             }
-            .padding()
         
             
                 

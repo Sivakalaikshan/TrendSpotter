5cf3b9ffeadfe0346f6868f8a4393e0c29c066b9
Sivakalaikshan <sivakalaikshan1999@gmail.com>
2024-03-23 18:34:28 +0530

    create cart design 2024-03-23

---
+++ TrendSpotter/Views/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  TrendSpotter
+//
+//  Created by NIBM on 2024-03-23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}

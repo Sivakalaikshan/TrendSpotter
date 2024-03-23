e59c84e13f1a024c4b3e6ecd397bf3781fa58b36
Sivakalaikshan <sivakalaikshan1999@gmail.com>
2024-03-23 12:33:23 +0530

    create cartview design 2024-03-23

---
+++ TrendSpotter/Views/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  TrendSpotter
+//
+//  Created by NIBM on 2024-03-23.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}

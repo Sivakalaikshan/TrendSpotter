83cd9d53435570edee4d31213b4307f55c14bfff
Sivakalaikshan <sivakalaikshan1999@gmail.com>
2024-03-29 20:11:04 +0530

    create signup and product details design 2024-03-29

---
+++ TrendSpotter/Views/SignupView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupView.swift
+//  TrendSpotter
+//
+//  Created by NIBMPC04PC03 on 2024-03-29.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupView()
+}

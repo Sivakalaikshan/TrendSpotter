918fe71793c357565cc77627cf28a166a0a80fcb
Sivakalaikshan <sivakalaikshan1999@gmail.com>
2024-03-31 16:41:59 +0530

    load datas with api 2024-03-31

---
+++ TrendSpotter/Views/SpalshView.swift
@@ -0,0 +1,18 @@
+//
+//  SpalshView.swift
+//  TrendSpotter
+//
+//  Created by NIBMPC04PC03 on 2024-03-31.
+//
+
+import SwiftUI
+
+struct SpalshView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpalshView()
+}

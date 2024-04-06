193d3af7508450c80da3ef1077d0bf6492225775
Sivakalaikshan <sivakalaikshan1999@gmail.com>
2024-04-06 17:57:00 +0530

    Add cart 2024-04-06

---
+++ TrendSpotter/Controller/NavigationManager.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationManager.swift
+//  TrendSpotter
+//
+//  Created by NIBM-LAB04-PC07 on 2024-04-06.
+//
+
+import Foundation
---
+++ TrendSpotter/Views/BrandView.swift
@@ -0,0 +1,18 @@
+//
+//  BrandView.swift
+//  TrendSpotter
+//
+//  Created by NIBM-LAB04-PC07 on 2024-04-06.
+//
+
+import SwiftUI
+
+struct BrandView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrandView()
+}
